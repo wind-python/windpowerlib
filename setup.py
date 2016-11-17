25a07d317919bb48677f748649d02d4dc4a68e5b
birgit <birgit.schachler@rl-institut.de>
2016-11-17 14:14:23 +0100

    Remove restriction to certain python versions from setup.py (see issue #2)

--- setup.py
+++ setup.py
@@ -6,10 +6,6 @@
 import sys
 from setuptools import setup
 
-# check python version.
-if not sys.version_info[:2] in ((2, 7), (3, 3), (3, 4)):
-    sys.exit('%s requires Python 2.7, 3.3, or 3.4' % 'feedinlib')
-
 setup(name='windpowerlib',
       version='0.0.2dev',
       description='Creating time series from wind power plants.',
