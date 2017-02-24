ac912662b053fe59ed8ada1c35c788c34c52bbfc
Birgit Schachler <RL-INSTITUT\birgit.schachler@solydxk>
2017-02-24 12:33:16 +0100

    Documentation changes in wind_speed module

--- windpowerlib/wind_speed.py
+++ windpowerlib/wind_speed.py
@@ -13,12 +13,13 @@ import numpy as np
 def logarithmic_wind_profile(v_wind, v_wind_height, hub_height, z_0,
                              obstacle_height=0):
     r"""
-    Calculates the wind speed at hub height with the logarithmic wind profile.
+    Calculates the wind speed at hub height using a logarithmic wind profile.
 
     The logarithmic height equation is used. There is the possibility of
     including the height of the surrounding obstacles in the calculation. This
-    fuction is carried out when the parameter `wind_model` of an object of the
-    class WindTurbine is 'logarithimc' or 'logarithimc_closest'.
+    function is carried out when the parameter `wind_model` of an instance of
+    the :class:`~.modelchain.Modelchain` class is 'logarithmic' or 
+    'logarithmic_closest'.
 
     Parameters
     ----------
@@ -31,13 +32,13 @@ def logarithmic_wind_profile(v_wind, v_wind_height, hub_height, z_0,
     z_0 : pandas.Series or array or float
         Roughness length.
     obstacle_height : float, optional
-        Height of obstacles in the surroundings of the wind turbine. Put
-        obstacle_height to zero for wide spread obstacles. Default: 0
+        Height of obstacles in the surrounding area of the wind turbine. Put
+        `obstacle_height` to zero for wide spread obstacles. Default: 0.
 
     Returns
     -------
     pandas.Series or array
-        Wind speed at hub height as time series.
+        Wind speed at hub height.
 
     Notes
     -----
@@ -48,19 +49,19 @@ def logarithmic_wind_profile(v_wind, v_wind_height, hub_height, z_0,
         \frac{h_{data}-d}{z_{0}}\right)}
 
     with:
-        v: wind speed, h: height, :math:`z_{0}`: roughness length
-        d: includes obstacle height (d = 0.7 * obstacle_height)
+        v: wind speed, h: height, :math:`z_{0}`: roughness length,
+        d: boundary layer offset (estimated by d = 0.7 * `obstacle_height`)
 
     For  d = 0 it results in the following equation [20]_, [25]_:
     
     .. math:: v_{wind,hub}=v_{wind,data}\cdot\frac{\ln\left(\frac{h_{hub}}
         {z_{0}}\right)}{\ln\left(\frac{h_{data}}{z_{0}}\right)}
 
-    :math:`h_{data}` is the height in which the wind speed
+    :math:`h_{data}` is the height at which the wind speed
     :math:`v_{wind,data}` is measured.
 
-    `v_wind_height`, `z_0`, `hub_height` and `obstacle_height` have to be of
-    the same unit.
+    Parameters `v_wind_height`, `z_0`, `hub_height` and `obstacle_height` have 
+    to be of the same unit.
 
     References
     ----------
@@ -74,8 +75,8 @@ def logarithmic_wind_profile(v_wind, v_wind_height, hub_height, z_0,
     """
     if 0.7 * obstacle_height > v_wind_height:
         raise ValueError('To take an obstacle height of ' +
-                         str(obstacle_height) + ' m into consideration wind ' +
-                         'speed data of a higher height is needed.')
+                         str(obstacle_height) + ' m into consideration, wind' +
+                         ' speed data of a higher height is needed.')
     return (v_wind * np.log((hub_height - 0.7 * obstacle_height) / z_0) /
             np.log((v_wind_height - 0.7 * obstacle_height) / z_0))
 
@@ -83,32 +84,32 @@ def logarithmic_wind_profile(v_wind, v_wind_height, hub_height, z_0,
 def v_wind_hellman(v_wind, v_wind_height, hub_height, hellman_exp=None,
                    z_0=None):
     r"""
-    Calculates the wind speed at hub height with the hellman equation.
+    Calculates the wind speed at hub height using the hellman equation.
 
     It is assumed that the wind profile follows a power law. This fuction is
-    carried out when the parameter `wind_model` of an object of the class
-    WindTurbine is 'hellman'.
+    carried out when the parameter `wind_model` of an instance of
+    the :class:`~.modelchain.Modelchain` class is 'hellman'.
 
     Parameters
     ----------
     v_wind : pandas.Series or array
         Wind speed time series.
     v_wind_height : float
-        Height for which the parameter `v_wind` applies.
+        Height for which the parameter v_wind` applies.
     hub_height : float
         Hub height of wind turbine.
     hellman_exp : float, optional
         The Hellman exponent, which combines the increase in wind speed due to
         stability of atmospheric conditions and surface roughness into one
-        constant. Default: hellman_exp = 1 / ln(h_hub/z_0). If no roughness
-        length is given hellman_exp = 1/7.
+        constant. Default: 1/7. If roughness length is given 
+        `hellman_exp` = 1 / ln(h_hub/z_0).
     z_0 : float, optional
-        Roughness length. Default: None
+        Roughness length. Default: None.
 
     Returns
     -------
     pandas.Series or array
-        Wind speed at hub height as time series.
+        Wind speed at hub height.
 
     Notes
     -----
@@ -126,7 +127,7 @@ def v_wind_hellman(v_wind, v_wind_height, hub_height, hellman_exp=None,
     the wind turbine.
 
     For the Hellman exponent :math:`\alpha` many studies use a value of 1/7 for
-    onshore and a value of 1/9 for for offshore. The Hellman exponent can also
+    onshore and a value of 1/9 for offshore. The Hellman exponent can also
     be calulated by the following equation [32]_, [33]_:
     
     .. math:: \alpha = \frac{1}{ln\left(\frac{h_{hub}}{z_0} \right)}
