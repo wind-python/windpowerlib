1fd0e19a02df0caf19efc456ded9163c44369383
Birgit Schachler <RL-INSTITUT\birgit.schachler@solydxk>
2017-04-10 18:53:43 +0200

    Adapt modelchain tests to bugfix

--- windpowerlib/tests/modelchain_tests.py
+++ windpowerlib/tests/modelchain_tests.py
@@ -1,6 +1,8 @@
 import windpowerlib.modelchain as mc
 import windpowerlib.wind_turbine as wt
 from nose.tools import eq_
+from pandas.util.testing import assert_series_equal
+import pandas as pd
 
 
 class TestModelchain:
@@ -9,8 +11,8 @@ class TestModelchain:
     def setUpClass(self):
         self.weather = {'temp_air': 267,
                         'temp_air_2': 266,
-                        'v_wind': 5.0,
-                        'v_wind_2': 4.0,
+                        'v_wind': pd.Series(data=[5.0]),
+                        'v_wind_2': pd.Series(data=[4.0]),
                         'pressure': 101125,
                         'z0': 0.15}
         self.data_height = {'temp_air': 2,
@@ -27,22 +29,26 @@ class TestModelchain:
                                 'temperature_model': 'interpolation',
                                 'power_output_model': 'cp_values',
                                 'density_corr': False}
+        self.test_mc = mc.Modelchain(self.test_wt, **self.test_modelchain)
 
     def test_v_wind_hub(self):
-        v_wind_exp = 5.0
+        v_wind_exp = pd.Series(data=[5.0])
         self.data_height['v_wind'] = 100
-        eq_(self.test_mc.v_wind_hub(self.weather, self.data_height),
+        assert_series_equal(
+            self.test_mc.v_wind_hub(self.weather, self.data_height),
             v_wind_exp)
 
-        v_wind_exp = 4.0
+        v_wind_exp = pd.Series(data=[4.0])
         self.data_height['v_wind'] = 10
         self.data_height['v_wind_2'] = 100
-        eq_(self.test_mc.v_wind_hub(self.weather, self.data_height),
+        assert_series_equal(
+            self.test_mc.v_wind_hub(self.weather, self.data_height),
             v_wind_exp)
 
-        v_wind_exp = 7.7413652271940308
+        v_wind_exp = pd.Series(data=[6.947477471865689])
         self.data_height['v_wind_2'] = 8
-        eq_(self.test_mc.v_wind_hub(self.weather, self.data_height),
+        assert_series_equal(
+            self.test_mc.v_wind_hub(self.weather, self.data_height),
             v_wind_exp)
 
     def test_rho_hub(self):
@@ -52,24 +58,24 @@ class TestModelchain:
             rho_exp)
 
         rho_exp = 1.30795205834766
-        self.data_height['temp_air'] = 10
+        self.data_height['temp_air'] = 2
         self.data_height['temp_air_2'] = 100
         eq_(self.test_mc.rho_hub(self.weather, self.data_height),
             rho_exp)
 
-        rho_exp = 1.305914635138703
-        self.data_height['temp_air_2'] = 8
+        rho_exp = 1.3657124534660552
+        self.data_height['temp_air_2'] = 10
         eq_(self.test_mc.rho_hub(self.weather, self.data_height),
             rho_exp)
 
     def test_run_model_1(self):
-        power_output_exp = 724829.76425940311
+        power_output_exp = pd.Series(data=[724829.76425940311])
         test_mc = mc.Modelchain(self.test_wt)
         test_mc.run_model(self.weather, self.data_height)
-        eq_(test_mc.power_output, power_output_exp)
+        assert_series_equal(test_mc.power_output, power_output_exp)
 
     def test_run_model_2(self):
-        power_output_exp = 539948.81543840829
+        power_output_exp = pd.Series(data=[539948.81543840829])
         test_mc = mc.Modelchain(self.test_wt, **self.test_modelchain)
         test_mc.run_model(self.weather, self.data_height)
-        eq_(test_mc.power_output, power_output_exp)
+        assert_series_equal(test_mc.power_output, power_output_exp)
