6f48f24c897b7f7c03159659ea5cdf73925aefff
SabineHaas <sabine.haas@rl-institut.de>
2017-04-13 15:08:41 +0200

    Make density_tests easier to read and delete unnecessary lines

--- windpowerlib/tests/density_tests.py
+++ windpowerlib/tests/density_tests.py
@@ -19,48 +19,54 @@ class TestDensityTemperature:
                             'pressure': 0}
 
     def test_temperature_gradient(self):
+        # Test pandas.Series
         T_hub_exp = pd.Series(data=[266.363, 267.36300])
         assert_series_equal(temperature_gradient(self.weather['temp_air'],
                                                  self.data_height['temp_air'],
-                                                 self.h_hub), T_hub_exp)
-
-        T_hub_exp = np.array([266.363, 267.36300])
+                                                 self.h_hub),
+                            T_hub_exp)
+        # Test array
         assert_array_equal(temperature_gradient(
             np.array(self.weather['temp_air']), self.data_height['temp_air'],
-            self.h_hub), T_hub_exp)
+                     self.h_hub), T_hub_exp)
 
     def test_temperature_interpol(self):
+        # Test pandas.Series
         T_hub_exp = pd.Series(data=[267.0, 243.5])
         assert_series_equal(temperature_interpol(
             self.weather['temp_air'], self.weather['temp_air_2'],
             self.data_height['temp_air'], self.data_height['temp_air_2'],
             self.h_hub), T_hub_exp)
-
-        T_hub_exp = np.array([267.0, 243.5])
+        # Test array
         assert_array_equal(temperature_interpol(
             np.array(self.weather['temp_air']),
             np.array(self.weather['temp_air_2']), self.data_height['temp_air'],
             self.data_height['temp_air_2'], self.h_hub), T_hub_exp)
 
     def test_rho_barometric(self):
+        # Test pandas.Series
         rho_exp = pd.Series(data=[1.30305336, 1.29656645])
-        assert_series_equal(
-            rho_barometric(self.weather['pressure'],
-                           self.data_height['pressure'], self.h_hub,
-                           self.weather['temp_air']), rho_exp)
-
-        rho_exp = np.array([1.30305336, 1.29656645])
-        assert_allclose(rho_barometric(
-            np.array(self.weather['pressure']), self.data_height['pressure'],
-            self.h_hub, np.array(self.weather['temp_air'])), rho_exp)
+        assert_series_equal(rho_barometric(self.weather['pressure'],
+                                           self.data_height['pressure'],
+                                           self.h_hub,
+                                           self.weather['temp_air']),
+                            rho_exp)
+        # Test array
+        assert_allclose(rho_barometric(np.array(self.weather['pressure']),
+                                       self.data_height['pressure'],
+                                       self.h_hub,
+                                       np.array(self.weather['temp_air'])),
+                        rho_exp)
 
     def test_rho_ideal_gas(self):
+        # Test pandas.Series
         rho_exp = pd.Series(data=[1.30309439, 1.29660728])
-        assert_series_equal(rho_ideal_gas(
-            self.weather['pressure'], self.data_height['pressure'],
-            self.h_hub, self.weather['temp_air']), rho_exp)
-
-        rho_exp = np.array([1.30309439, 1.29660728])
+        assert_series_equal(rho_ideal_gas(self.weather['pressure'],
+                                          self.data_height['pressure'],
+                                          self.h_hub,
+                                          self.weather['temp_air']),
+                            rho_exp)
+        # Test array
         assert_allclose(rho_ideal_gas(
             np.array(self.weather['pressure']), self.data_height['pressure'],
             self.h_hub, np.array(self.weather['temp_air'])), rho_exp)
