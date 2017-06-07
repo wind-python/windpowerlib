b6d95cc0a34537174d405ff9e2f765400f1eaf8a
SabineHaas <sabine.haas@rl-institut.de>
2017-06-07 13:19:16 +0200

    Concistency tests for wind_speed functions

--- tests/test_wind_speed.py
+++ tests/test_wind_speed.py
@@ -14,19 +14,36 @@ class TestWindSpeed:
                       'hub_height': 100,
                       'z_0': pd.Series(data=[0.15, 0.15]),
                       'obstacle_height': 0}
-        # Test pandas.Series
+
+        # Test v_wind as pd.Series with z_0 as pd.Series, np.array and float
         v_wind_hub_exp = pd.Series(data=[7.74136523, 10.0637748])
         assert_series_equal(logarithmic_wind_profile(**parameters),
                             v_wind_hub_exp)
-        # Test numpy array
         parameters['z_0'] = np.array(parameters['z_0'])
+        assert_series_equal(logarithmic_wind_profile(**parameters),
+                            v_wind_hub_exp)
+        parameters['z_0'] = parameters['z_0'][0]
+        assert_series_equal(logarithmic_wind_profile(**parameters),
+                            v_wind_hub_exp)
+
+        # Test v_wind as np.array with z_0 as float, pd.Series and np.array
+        v_wind_hub_exp = np.array([7.74136523, 10.0637748])
         parameters['v_wind'] = np.array(parameters['v_wind'])
         assert_allclose(logarithmic_wind_profile(**parameters), v_wind_hub_exp)
-        # Test z_0 is float and obstacle height
+        assert isinstance(logarithmic_wind_profile(**parameters), np.ndarray)
+        parameters['z_0'] = pd.Series(data=[parameters['z_0'],
+                                      parameters['z_0']])
+        assert_allclose(logarithmic_wind_profile(**parameters), v_wind_hub_exp)
+        assert isinstance(logarithmic_wind_profile(**parameters), np.ndarray)
+        parameters['z_0'] = np.array(parameters['z_0'])
+        assert_allclose(logarithmic_wind_profile(**parameters), v_wind_hub_exp)
+        assert isinstance(logarithmic_wind_profile(**parameters), np.ndarray)
+
+        # Test obstacle height is not zero
         v_wind_hub_exp = np.array([13.54925281, 17.61402865])
-        parameters['z_0'] = parameters['z_0'][0]
         parameters['obstacle_height'] = 12
         assert_allclose(logarithmic_wind_profile(**parameters), v_wind_hub_exp)
+
         # Raise ValueError due to 0.7 * `obstacle_height` > `v_wind_height`
         with pytest.raises(ValueError):
             parameters['obstacle_height'] = 20
@@ -36,30 +53,41 @@ class TestWindSpeed:
         parameters = {'v_wind': pd.Series(data=[5.0, 6.5]),
                       'v_wind_height': 10,
                       'hub_height': 100,
-                      'z_0': None,
+                      'z_0': pd.Series(data=[0.15, 0.15]),
                       'hellman_exp': None}
-        # Test pandas.Series and z_0 is None
-        v_wind_hub_exp = pd.Series(data=[6.9474774, 9.03172])
-        assert_series_equal(v_wind_hellman(**parameters), v_wind_hub_exp)
 
-        # Test z_0 is float
+        # Test v_wind is pd.Series with z_0 is pd.Series, np.array and float
         v_wind_hub_exp = pd.Series(data=[7.12462437, 9.26201168])
-        parameters['z_0'] = 0.15
+        assert_series_equal(v_wind_hellman(**parameters), v_wind_hub_exp)
+        parameters['z_0'] = np.array(parameters['z_0'])
+        assert_series_equal(v_wind_hellman(**parameters), v_wind_hub_exp)
+        parameters['z_0'] = parameters['z_0'][0]
         assert_series_equal(v_wind_hellman(**parameters), v_wind_hub_exp)
 
-        # Test array and z_0 is pandas.Series
+        # Test v_wind as np.array with z_0 is float, pd.Series and np.array
+        v_wind_hub_exp = np.array([7.12462437, 9.26201168])
         parameters['v_wind'] = np.array(parameters['v_wind'])
-        parameters['z_0'] = pd.Series(data=[0.15, 0.15])
         assert_allclose(v_wind_hellman(**parameters), v_wind_hub_exp)
-
-        # Test z_0 is array
+        assert isinstance(v_wind_hellman(**parameters), np.ndarray)
+        parameters['z_0'] = pd.Series(data=(parameters['z_0'],
+                                            parameters['z_0']))
+        assert_allclose(v_wind_hellman(**parameters), v_wind_hub_exp)
+        assert isinstance(v_wind_hellman(**parameters), np.ndarray)
         parameters['z_0'] = np.array(parameters['z_0'])
         assert_allclose(v_wind_hellman(**parameters), v_wind_hub_exp)
+        assert isinstance(v_wind_hellman(**parameters), np.ndarray)
+
+        # Test z_0 is None and hellman_exp is None
+        v_wind_hub_exp = pd.Series(data=[6.9474774, 9.03172])
+        parameters['v_wind'] = pd.Series(data=parameters['v_wind'])
+        parameters['z_0'] = None
+        assert_series_equal(v_wind_hellman(**parameters), v_wind_hub_exp)
 
         # Test hellman_exp is not None
-        v_wind_hub_exp = np.array([7.92446596, 10.30180575])
+        v_wind_hub_exp = pd.Series(data=[7.92446596, 10.30180575])
+        parameters['z_0'] = 0.15
         parameters['hellman_exp'] = 0.2
-        assert_allclose(v_wind_hellman(**parameters), v_wind_hub_exp)
+        assert_series_equal(v_wind_hellman(**parameters), v_wind_hub_exp)
 
         # Raise TypeErrors due to wrong types of `hellman_exp`.
         parameters['hellman_exp'] = 8
