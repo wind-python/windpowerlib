c5f3df771c6b112bc38a418d9b18f7caf66bb44b
SabineHaas <sabine.haas@rl-institut.de>
2017-06-07 16:23:16 +0200

    Consistency tests for power_output functions

--- tests/test_power_output.py
+++ tests/test_power_output.py
@@ -19,50 +19,93 @@ class TestPowerOutput:
                                      index=[4.0, 5.0, 6.0])
 
     def test_cp_curve(self):
-        # Test pandas.Series
+        parameters = {'v_wind': pd.Series(data=[2.0, 5.5, 7.0]),
+                      'rho_hub': pd.Series(data=[1.3, 1.3, 1.3]),
+                      'd_rotor': 80,
+                      'cp_values': pd.DataFrame(data={'cp': [0.3, 0.4, 0.5]},
+                                                index=[4.0, 5.0, 6.0])}
+
+        # Test v_wind as pd.Series with rho_hub as pd.Series and np.array
         power_output_exp = pd.Series(data=[0.0, 244615.399, 0.0],
                                      name='feedin_wind_turbine')
-        assert_series_equal(cp_curve(self.v_wind, self.rho_hub, self.d_rotor,
-                                     self.cp_values),
-                            power_output_exp)
-        # Test numpy array
-        assert_allclose(cp_curve(np.array(self.v_wind), np.array(self.rho_hub),
-                                 self.d_rotor, self.cp_values),
-                        power_output_exp)
+        assert_series_equal(cp_curve(**parameters), power_output_exp)
+        parameters['rho_hub'] = np.array(parameters['rho_hub'])
+        assert_series_equal(cp_curve(**parameters), power_output_exp)
+
+        # Test v_wind as np.array with rho_hub as np.array and pd.Series
+        power_output_exp = np.array([0.0, 244615.399, 0.0])
+        parameters['v_wind'] = np.array(parameters['v_wind'])
+        assert_allclose(cp_curve(**parameters), power_output_exp)
+        assert isinstance(cp_curve(**parameters), np.ndarray)
+        parameters['rho_hub'] = pd.Series(data=parameters['rho_hub'])
+        assert_allclose(cp_curve(**parameters), power_output_exp)
+        assert isinstance(cp_curve(**parameters), np.ndarray)
 
     def test_cp_curve_density_corrected(self):
-        # Test pandas.Series
+        parameters = {'v_wind': pd.Series(data=[2.0, 5.5, 7.0]),
+                      'rho_hub': pd.Series(data=[1.3, 1.3, 1.3]),
+                      'd_rotor': 80,
+                      'cp_values': pd.DataFrame(data={'cp': [0.3, 0.4, 0.5]},
+                                                index=[4.0, 5.0, 6.0])}
+
+        # Test v_wind as pd.Series with rho_hub as pd.Series and np.array
         power_output_exp = pd.Series(data=[0.0, 262869.785, 0.0],
                                      name='feedin_wind_turbine')
-        assert_series_equal(cp_curve_density_corr(self.v_wind, self.rho_hub,
-                                                  self.d_rotor,
-                                                  self.cp_values),
+        assert_series_equal(cp_curve_density_corr(**parameters),
                             power_output_exp)
-        # Test numpy array
-        assert_allclose(cp_curve_density_corr(np.array(self.v_wind),
-                                              np.array(self.rho_hub),
-                                              self.d_rotor, self.cp_values),
+        parameters['rho_hub'] = np.array(parameters['rho_hub'])
+        assert_series_equal(cp_curve_density_corr(**parameters),
+                            power_output_exp)
+
+        # Test v_wind as np.array with rho_hub as np.array and pd.Series
+        power_output_exp = np.array([0.0, 262869.785, 0.0])
+        parameters['v_wind'] = np.array(parameters['v_wind'])
+        assert_allclose(cp_curve_density_corr(**parameters),
                         power_output_exp)
+        assert isinstance(cp_curve_density_corr(**parameters), np.ndarray)
+        parameters['rho_hub'] = pd.Series(data=parameters['rho_hub'])
+        assert_allclose(cp_curve_density_corr(**parameters),
+                        power_output_exp)
+        assert isinstance(cp_curve_density_corr(**parameters), np.ndarray)
 
     def test_p_curve(self):
-        # Test pandas.Series
+        parameters = {'v_wind': pd.Series(data=[2.0, 5.5, 7.0]),
+                      'p_values': pd.DataFrame(data={'p': [300, 400, 500]},
+                                               index=[4.0, 5.0, 6.0])}
+
+        # Test v_wind as pd.Series
         power_output_exp = pd.Series(data=[0.0, 450.0, 0.0],
                                      name='feedin_wind_turbine')
-        assert_series_equal(p_curve(self.v_wind, self.p_values),
-                            power_output_exp)
-        # Test numpy array
-        assert_allclose(p_curve(np.array(self.v_wind), self.p_values),
-                        power_output_exp)
+        assert_series_equal(p_curve(**parameters), power_output_exp)
+
+        # Test v_wind as np.array
+        power_output_exp = np.array([0.0, 450.0, 0.0])
+        parameters['v_wind'] = np.array(parameters['v_wind'])
+        assert_allclose(p_curve(**parameters), power_output_exp)
+        assert isinstance(p_curve(**parameters), np.ndarray)
 
     def test_p_curve_density_corrected(self):
-        # Test pandas.Series
+        parameters = {'v_wind': pd.Series(data=[2.0, 5.5, 7.0]),
+                      'rho_hub': pd.Series(data=[1.3, 1.3, 1.3]),
+                      'p_values': pd.DataFrame(data={'p': [300, 400, 500]},
+                                               index=[4.0, 5.0, 6.0])}
+
+         # Test v_wind as pd.Series with rho_hub as pd.Series and np.array
         power_output_exp = pd.Series(data=[0.0, 461.00290572, 0.0],
                                      name='feedin_wind_turbine')
-        assert_series_equal(p_curve_density_corr(self.v_wind, self.rho_hub,
-                                                 self.p_values),
+        assert_series_equal(p_curve_density_corr(**parameters),
                             power_output_exp)
-        # Test numpy array
-        assert_allclose(p_curve_density_corr(np.array(self.v_wind),
-                                             np.array(self.rho_hub),
-                                             self.p_values),
+        parameters['rho_hub'] = np.array(parameters['rho_hub'])
+        assert_series_equal(p_curve_density_corr(**parameters),
+                            power_output_exp)
+
+        # Test v_wind as np.array with rho_hub as np.array and pd.Series
+        power_output_exp = np.array([0.0, 461.00290572, 0.0])
+        parameters['v_wind'] = np.array(parameters['v_wind'])
+        assert_allclose(p_curve_density_corr(**parameters),
                         power_output_exp)
+        assert isinstance(p_curve_density_corr(**parameters), np.ndarray)
+        parameters['rho_hub'] = pd.Series(data=parameters['rho_hub'])
+        assert_allclose(p_curve_density_corr(**parameters),
+                        power_output_exp)
+        assert isinstance(p_curve_density_corr(**parameters), np.ndarray)
