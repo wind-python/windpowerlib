4fb56b6a0c0ee4930ae2ae883de81e6cb2f5e76e
SabineHaas <sabine.haas@rl-institut.de>
2017-06-15 11:42:19 +0200

    Correction of docstrings

--- windpowerlib/tools.py
+++ windpowerlib/tools.py
@@ -35,9 +35,9 @@ def smallest_difference(data_frame, comp_value, column_name):
 
     Returns
     -------
-    Tuple(float, pd.Series or np.array or float)
+    Tuple(float, float or pd.Series or np.array)
         Closest value to comparative value as float and its corresponding value
-        as float.
+        as float, pd.Series or np.array.
 
     """
     diff = []
@@ -53,15 +53,15 @@ def linear_extra_interpolation(data_frame, requested_height, column_name):
     Inter- or extrapolates between the values of a data frame.
 
     This function can for example be used for the interpolation of a wind
-    speed, density or temperature.
+    speed, density or temperature to calculated these values at hub height of a
+    wind turbine.
 
     Parameters
     ----------
     data_frame : DataFrame
         Indices are the values between which will be interpolated or from which
         will be extrapolated, the corresponding values are in the column
-        specified by `column_name` and they can be floats, pd.Series or
-        np.arrays.
+        specified by `column_name` and can be floats, pd.Series or np.arrays.
     requested_height : float
         Height for which the interpolation takes place (e.g. hub height of wind
         turbine).
@@ -71,21 +71,21 @@ def linear_extra_interpolation(data_frame, requested_height, column_name):
 
     Returns
     -------
-    interpolant : pandas.Series, numpy.array or float
+    interpolant : float or pandas.Series or numpy.array
         Result of the interpolation (e.g. density at hub height).
 
     Notes
     -----
 
-    For the interpolation np.interp() is used and the following equation is
-    used for extrapolation:
+    For the inter- and extrapolation the following equation is used:
 
     .. math:: interpolant = (value_2 - value_1) / (height_2 - height_1) *
               (height_{requested} - height_1) + value_1
 
     with:
-        :math:`height_2`: largest/smallest index of data frame,
-        :math:`height_1`: second largest/smallest index of data frame,
+        :math:`height_2`: index of data frame closest to
+        :math:`height_{requested}`, :math:`height_1`: index of data frame
+        second closest to :math:`height_{requested}`,
         :math:`value_2`: corresponding value to `height_2`,
         :math:`value_1`: correponding value to `height_1`,
         :math:`height_{requested}` : height for which the interpolation takes
