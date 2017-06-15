5dadfe4b53920e2b49f7a82083362e96e9864e04
SabineHaas <sabine.haas@rl-institut.de>
2017-06-15 11:32:14 +0200

    Rename variable for consistency

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -8,58 +8,58 @@ from numpy.testing import assert_array_equal
 class TestTools:
 
     def test_smallest_difference(self):
-        data_frame = pd.DataFrame(data={'v_wind': [4.0, 5.0, 6.0]},
-                                  index=[100, 150, 200])
-        data_frame_series = pd.DataFrame(data={'v_wind': [
+        weather = pd.DataFrame(data={'v_wind': [4.0, 5.0, 6.0]},
+                               index=[100, 150, 200])
+        weather_series = pd.DataFrame(data={'v_wind': [
             pd.Series(data=[4.0, 5.0, 6.0]),
             pd.Series(data=[8.0, 10.0, 14.0]),
             pd.Series(data=[16.0, 20.0, 28.0])]}, index=[100, 150, 200])
-        data_frame_arr = pd.DataFrame(data={'v_wind': [
-            np.array(data_frame_series['v_wind'][data_frame_series.index[0]]),
-            np.array(data_frame_series['v_wind'][data_frame_series.index[1]]),
-            np.array(data_frame_series['v_wind'][data_frame_series.index[2]])]},
-                                   index=data_frame_series.index)
+        weather_arr = pd.DataFrame(data={'v_wind': [
+            np.array(weather_series['v_wind'][weather_series.index[0]]),
+            np.array(weather_series['v_wind'][weather_series.index[1]]),
+            np.array(weather_series['v_wind'][weather_series.index[2]])]},
+                                   index=weather_series.index)
         parameters = {'comp_value': 100,
                       'column_name': 'v_wind'}
 
         # comparative value is an index of data frame
         exp_output = (100, 4.0)
-        assert smallest_difference(data_frame, **parameters) == exp_output
+        assert smallest_difference(weather, **parameters) == exp_output
         exp_series = pd.Series(data=[4.0, 5.0, 6.0])
-        assert_series_equal(smallest_difference(data_frame_series,
+        assert_series_equal(smallest_difference(weather_series,
                                                 **parameters)[1], exp_series)
         exp_arr = np.array(exp_series)
-        assert_array_equal(smallest_difference(data_frame_arr,
+        assert_array_equal(smallest_difference(weather_arr,
                                                **parameters)[1], exp_arr)
         # comparative value between indices of data frame
         exp_output = (150, 5.0)
         parameters['comp_value'] = 175
-        assert smallest_difference(data_frame, **parameters) == exp_output
+        assert smallest_difference(weather, **parameters) == exp_output
         exp_series = pd.Series(data=[8.0, 10.0, 14.0])
-        assert_series_equal(smallest_difference(data_frame_series,
+        assert_series_equal(smallest_difference(weather_series,
                                                 **parameters)[1], exp_series)
         exp_arr = np.array(exp_series)
-        assert_array_equal(smallest_difference(data_frame_arr,
+        assert_array_equal(smallest_difference(weather_arr,
                                                **parameters)[1], exp_arr)
         # comparative value > indices of data frame
         exp_output = (200, 6.0)
         parameters['comp_value'] = 250
-        assert smallest_difference(data_frame, **parameters) == exp_output
+        assert smallest_difference(weather, **parameters) == exp_output
         exp_series = pd.Series(data=[16.0, 20.0, 28.0])
-        assert_series_equal(smallest_difference(data_frame_series,
+        assert_series_equal(smallest_difference(weather_series,
                                                 **parameters)[1], exp_series)
         exp_arr = np.array(exp_series)
-        assert_array_equal(smallest_difference(data_frame_arr,
+        assert_array_equal(smallest_difference(weather_arr,
                                                **parameters)[1], exp_arr)
         # comparative value < indices of data frame
         exp_output = (100, 4.0)
         parameters['comp_value'] = 90
-        assert smallest_difference(data_frame, **parameters) == exp_output
+        assert smallest_difference(weather, **parameters) == exp_output
         exp_series = pd.Series(data=[4.0, 5.0, 6.0])
-        assert_series_equal(smallest_difference(data_frame_series,
+        assert_series_equal(smallest_difference(weather_series,
                                                 **parameters)[1], exp_series)
         exp_arr = np.array(exp_series)
-        assert_array_equal(smallest_difference(data_frame_arr,
+        assert_array_equal(smallest_difference(weather_arr,
                                                **parameters)[1], exp_arr)
 
     def test_linear_extra_interpolation(self):
