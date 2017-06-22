99c40bf2e9d5837f21367ccc7b4477a14b354c26
SabineHaas <sabine.haas@rl-institut.de>
2017-06-22 11:23:34 +0200

    Adapt tests to API changes

--- tests/test_density.py
+++ tests/test_density.py
@@ -1,29 +1,13 @@
-from windpowerlib.density import (temperature_gradient,
-                                  rho_barometric, rho_ideal_gas)
+from windpowerlib.density import barometric, ideal_gas
 import pandas as pd
 from pandas.util.testing import assert_series_equal
-from numpy.testing import assert_array_equal, assert_allclose
+from numpy.testing import assert_allclose
 import numpy as np
 
 
-class TestDensityTemperature:
+class TestDensity:
 
-    def test_temperature_gradient(self):
-        parameters = {'temperature': pd.Series(data=[267, 268]),
-                      'temperature_height': 2,
-                      'hub_height': 100}
-
-        # Test temperature as pd.Series
-        temp_hub_exp = pd.Series(data=[266.363, 267.36300])
-        assert_series_equal(temperature_gradient(**parameters), temp_hub_exp)
-
-        # Test temperature as np.array
-        temp_hub_exp = np.array([266.363, 267.36300])
-        parameters['temperature'] = np.array(parameters['temperature'])
-        assert_array_equal(temperature_gradient(**parameters), temp_hub_exp)
-        assert isinstance(temperature_gradient(**parameters), np.ndarray)
-
-    def test_rho_barometric(self):
+    def test_barometric(self):
         parameters = {'pressure': pd.Series(data=[101125, 101000]),
                       'pressure_height': 0,
                       'hub_height': 100,
@@ -32,25 +16,25 @@ class TestDensityTemperature:
         # Test pressure as pd.Series and temperature_hub_height as pd.Series
         # and np.array
         rho_exp = pd.Series(data=[1.30305336, 1.29656645])
-        assert_series_equal(rho_barometric(**parameters), rho_exp)
+        assert_series_equal(barometric(**parameters), rho_exp)
         parameters['temperature_hub_height'] = np.array(
             parameters['temperature_hub_height'])
-        assert_series_equal(rho_barometric(**parameters), rho_exp)
+        assert_series_equal(barometric(**parameters), rho_exp)
 
         # Test pressure as np.array and temperature_hub_height as pd.Series
         parameters['pressure'] = np.array(parameters['pressure'])
         parameters['temperature_hub_height'] = pd.Series(
             data=parameters['temperature_hub_height'])
-        assert_series_equal(rho_barometric(**parameters), rho_exp)
+        assert_series_equal(barometric(**parameters), rho_exp)
 
         # Test pressure as np.array and temperature_hub_height as np.array
         rho_exp = np.array([1.30305336, 1.29656645])
         parameters['temperature_hub_height'] = np.array(
             parameters['temperature_hub_height'])
-        assert_allclose(rho_barometric(**parameters), rho_exp)
-        assert isinstance(rho_barometric(**parameters), np.ndarray)
+        assert_allclose(barometric(**parameters), rho_exp)
+        assert isinstance(barometric(**parameters), np.ndarray)
 
-    def test_rho_ideal_gas(self):
+    def test_ideal_gas(self):
         parameters = {'pressure': pd.Series(data=[101125, 101000]),
                       'pressure_height': 0,
                       'hub_height': 100,
@@ -59,20 +43,20 @@ class TestDensityTemperature:
         # Test pressure as pd.Series and temperature_hub_height as pd.Series
         # and np.array
         rho_exp = pd.Series(data=[1.30309439, 1.29660728])
-        assert_series_equal(rho_ideal_gas(**parameters), rho_exp)
+        assert_series_equal(ideal_gas(**parameters), rho_exp)
         parameters['temperature_hub_height'] = np.array(
             parameters['temperature_hub_height'])
-        assert_series_equal(rho_ideal_gas(**parameters), rho_exp)
+        assert_series_equal(ideal_gas(**parameters), rho_exp)
 
         # Test pressure as np.array and temperature_hub_height as pd.Series
         parameters['pressure'] = np.array(parameters['pressure'])
         parameters['temperature_hub_height'] = pd.Series(
             data=parameters['temperature_hub_height'])
-        assert_allclose(rho_ideal_gas(**parameters), rho_exp)
+        assert_allclose(ideal_gas(**parameters), rho_exp)
 
         # Test pressure as np.array and temperature_hub_height as np.array
         rho_exp = np.array([1.30309439, 1.29660728])
         parameters['temperature_hub_height'] = np.array(
             parameters['temperature_hub_height'])
-        assert_allclose(rho_ideal_gas(**parameters), rho_exp)
-        assert isinstance(rho_ideal_gas(**parameters), np.ndarray)
+        assert_allclose(ideal_gas(**parameters), rho_exp)
+        assert isinstance(ideal_gas(**parameters), np.ndarray)
--- tests/test_power_output.py
+++ tests/test_power_output.py
@@ -1,6 +1,7 @@
 from pandas.util.testing import assert_series_equal
-from windpowerlib.power_output import (cp_curve, cp_curve_density_corr,
-                                       p_curve, p_curve_density_corr)
+from windpowerlib.power_output import (power_coefficient_curve,
+                                       cp_curve_density_corr,
+                                       power_curve, p_curve_density_corr)
 import pandas as pd
 import numpy as np
 from numpy.testing import assert_allclose
@@ -8,7 +9,7 @@ from numpy.testing import assert_allclose
 
 class TestPowerOutput:
 
-    def test_cp_curve(self):
+    def test_power_coefficient_curve(self):
         parameters = {'wind_speed': pd.Series(data=[2.0, 5.5, 7.0]),
                       'density': pd.Series(data=[1.3, 1.3, 1.3]),
                       'rotor_diameter': 80,
@@ -18,18 +19,22 @@ class TestPowerOutput:
         # Test wind_speed as pd.Series with density as pd.Series and np.array
         power_output_exp = pd.Series(data=[0.0, 244615.399, 0.0],
                                      name='feedin_wind_turbine')
-        assert_series_equal(cp_curve(**parameters), power_output_exp)
+        assert_series_equal(power_coefficient_curve(**parameters),
+                            power_output_exp)
         parameters['density'] = np.array(parameters['density'])
-        assert_series_equal(cp_curve(**parameters), power_output_exp)
+        assert_series_equal(power_coefficient_curve(**parameters),
+                            power_output_exp)
 
         # Test wind_speed as np.array with density as np.array and pd.Series
         power_output_exp = np.array([0.0, 244615.399, 0.0])
         parameters['wind_speed'] = np.array(parameters['wind_speed'])
-        assert_allclose(cp_curve(**parameters), power_output_exp)
-        assert isinstance(cp_curve(**parameters), np.ndarray)
+        assert_allclose(power_coefficient_curve(**parameters),
+                        power_output_exp)
+        assert isinstance(power_coefficient_curve(**parameters), np.ndarray)
         parameters['density'] = pd.Series(data=parameters['density'])
-        assert_allclose(cp_curve(**parameters), power_output_exp)
-        assert isinstance(cp_curve(**parameters), np.ndarray)
+        assert_allclose(power_coefficient_curve(**parameters),
+                        power_output_exp)
+        assert isinstance(power_coefficient_curve(**parameters), np.ndarray)
 
     def test_cp_curve_density_corrected(self):
         parameters = {'wind_speed': pd.Series(data=[2.0, 5.5, 7.0]),
@@ -58,7 +63,7 @@ class TestPowerOutput:
                         power_output_exp)
         assert isinstance(cp_curve_density_corr(**parameters), np.ndarray)
 
-    def test_p_curve(self):
+    def test_power_curve(self):
         parameters = {'wind_speed': pd.Series(data=[2.0, 5.5, 7.0]),
                       'p_values': pd.DataFrame(data={'p': [300, 400, 500]},
                                                index=[4.0, 5.0, 6.0])}
@@ -66,13 +71,13 @@ class TestPowerOutput:
         # Test wind_speed as pd.Series
         power_output_exp = pd.Series(data=[0.0, 450.0, 0.0],
                                      name='feedin_wind_turbine')
-        assert_series_equal(p_curve(**parameters), power_output_exp)
+        assert_series_equal(power_curve(**parameters), power_output_exp)
 
         # Test wind_speed as np.array
         power_output_exp = np.array([0.0, 450.0, 0.0])
         parameters['wind_speed'] = np.array(parameters['wind_speed'])
-        assert_allclose(p_curve(**parameters), power_output_exp)
-        assert isinstance(p_curve(**parameters), np.ndarray)
+        assert_allclose(power_curve(**parameters), power_output_exp)
+        assert isinstance(power_curve(**parameters), np.ndarray)
 
     def test_p_curve_density_corrected(self):
         parameters = {'wind_speed': pd.Series(data=[2.0, 5.5, 7.0]),
