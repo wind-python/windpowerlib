207569d07a30b7492ec7a89e65fa56c09f8a0513
birgit <birgit.schachler@rl-institut.de>
2017-06-30 18:53:47 +0200

    Rename and change type of WindTurbine attributes p_values and cp_values

--- windpowerlib/wind_turbine.py
+++ windpowerlib/wind_turbine.py
@@ -29,21 +29,20 @@ class WindTurbine(object):
         Hub height of the wind turbine in m.
     rotor_diameter : None or float
         Diameter of the rotor in m.
-    cp_values : None or pandas.DataFrame
-        Power coefficient curve of the wind turbine.
-        The indices of the DataFrame are the corresponding wind speeds of the
-        power coefficient curve, the power coefficient values are listed in
-        the column 'cp'. Default: None.
-    p_values : None or pandas.DataFrame
-        Power curve of the wind turbine.
-        The indices of the DataFrame are the corresponding wind speeds of the
-        power curve, the power values are listed in the column 'p'.
-        Default: None.
+    power_coefficient_curve : None, pandas.DataFrame or dictionary
+        Power coefficient curve of the wind turbine. DataFrame/dictionary must
+        have 'wind_speed' and 'values' columns/keys with wind speeds in m/s
+        and the corresponding power coefficients. Default: None.
+    power_curve : None, pandas.DataFrame or dictionary
+        Power curve of the wind turbine. DataFrame/dictionary must have
+        'wind_speed' and 'values' columns/keys with wind speeds in m/s and the
+        corresponding power curve value in W. Default: None.
     nominal_power : None or float
         The nominal output of the wind turbine in W.
     fetch_curve : string
         Parameter to specify whether the power or power coefficient curve
-        should be retrieved from the provided turbine data. Default: 'p'.
+        should be retrieved from the provided turbine data. Valid options are
+        'power_curve' and 'power_coefficient_curve'. Default: 'power_curve'.
 
     Attributes
     ----------
@@ -55,21 +54,20 @@ class WindTurbine(object):
         Hub height of the wind turbine in m.
     rotor_diameter : None or float
         Diameter of the rotor in m.
-    cp_values : None or pandas.DataFrame
-        Power coefficient curve of the wind turbine.
-        The indices of the DataFrame are the corresponding wind speeds of the
-        power coefficient curve, the power coefficient values are listed in
-        the column 'cp'. Default: None.
-    p_values : None or pandas.DataFrame
-        Power curve of the wind turbine.
-        Indices are the wind speeds of the power curve in m/s, the
-        corresponding power values in W are in the column 'p'.
-        Default: None.
+    power_coefficient_curve : None, pandas.DataFrame or dictionary
+        Power coefficient curve of the wind turbine. DataFrame/dictionary must
+        have 'wind_speed' and 'values' columns/keys with wind speeds in m/s
+        and the corresponding power coefficients. Default: None.
+    power_curve : None, pandas.DataFrame or dictionary
+        Power curve of the wind turbine. DataFrame/dictionary must have
+        'wind_speed' and 'values' columns/keys with wind speeds in m/s and the
+        corresponding power curve value in W. Default: None.
     nominal_power : None or float
         The nominal output of the wind turbine in W.
     fetch_curve : string
         Parameter to specify whether the power or power coefficient curve
-        should be retrieved from the provided turbine data. Default: 'p'.
+        should be retrieved from the provided turbine data. Valid options are
+        'power_curve' and 'power_coefficient_curve'. Default: 'power_curve'.
     power_output : pandas.Series
         The calculated power output of the wind turbine.
 
@@ -87,20 +85,20 @@ class WindTurbine(object):
     """
 
     def __init__(self, turbine_name, hub_height, rotor_diameter=None,
-                 cp_values=None, p_values=None, nominal_power=None,
-                 fetch_curve='p'):
+                 power_coefficient_curve=None, power_curve=None,
+                 nominal_power=None, fetch_curve='power_curve'):
 
         self.turbine_name = turbine_name
         self.hub_height = hub_height
         self.rotor_diameter = rotor_diameter
-        self.cp_values = cp_values
-        self.p_values = p_values
+        self.power_coefficient_curve = power_coefficient_curve
+        self.power_curve = power_curve
         self.nominal_power = nominal_power
         self.fetch_curve = fetch_curve
 
         self.power_output = None
 
-        if self.cp_values is None and self.p_values is None:
+        if self.power_coefficient_curve is None and self.power_curve is None:
             self.fetch_turbine_data()
 
     def fetch_turbine_data(self):
@@ -112,7 +110,7 @@ class WindTurbine(object):
         You can also use this function to import your own power (coefficient)
         curves. Therefore the wind speeds in m/s have to be in the first row
         and the corresponding power coefficient curve values or power
-        curve values in kW in a row where the first column contains the turbine
+        curve values in W in a row where the first column contains the turbine
         name (See directory windpowerlib/data as reference).
 
         Returns
@@ -149,7 +147,7 @@ class WindTurbine(object):
                 and nominal power (float).
                 Power (coefficient) curve DataFrame contains power coefficient
                 curve values (dimensionless) or power curve values in W with
-                the corresponding wind speeds in m/s as indices.
+                the corresponding wind speeds in m/s.
 
             """
             df = read_turbine_data(filename=filename)
@@ -208,14 +206,16 @@ def read_turbine_data(**kwargs):
     -------
     pandas.DataFrame
         Power coefficient curve values (dimensionless) or power curve values
-        in kW with the corresponding wind speeds in m/s as indices.
+        in kW with corresponding wind speeds in m/s of all available wind
+        turbines with turbine name in column 'turbine_id', turbine nominal
+        power in column 'p_nom'.
 
     """
     if 'datapath' not in kwargs:
         kwargs['datapath'] = os.path.join(os.path.dirname(__file__), 'data')
 
     if 'filename' not in kwargs:
-        kwargs['filename'] = 'p_curves.csv'
+        kwargs['filename'] = 'power_curves.csv'
 
     df = pd.read_csv(os.path.join(kwargs['datapath'], kwargs['filename']),
                      index_col=0)
@@ -231,7 +231,7 @@ def get_turbine_types(print_out=True, **kwargs):
     Parameters
     ----------
     print_out : boolean
-        Directly prints the list of types if set to True. Default: True
+        Directly prints the list of types if set to True. Default: True.
 
     Examples
     --------
