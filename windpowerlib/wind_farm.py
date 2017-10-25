e16f7f89c25e8ae746e3ed811172e566920f5a2e
SabineHaas <sabine.haas@rl-institut.de>
2017-10-25 16:45:28 +0200

    Add wind_farm module

---
+++ windpowerlib/wind_farm.py
@@ -0,0 +1,31 @@
+"""
+The ``wind_farm`` module contains the class WindFarm that implements
+a wind farm in the windpowerlib and functions needed for the modelling of a
+wind farm.
+
+"""
+
+__copyright__ = "Copyright oemof developer group"
+__license__ = "GPLv3"
+
+import numpy as np
+
+
+class WindFarm(object):
+    """
+    
+    """
+    def __init__(self, wind_farm_name, wind_turbine_fleet, coordinates,
+                 power_curve=None, power_output=None):
+
+        self.wind_farm_name = wind_farm_name
+        self.wind_turbine_fleet = wind_turbine_fleet
+        self.coordinates = coordinates
+
+        self.power_curve = power_curve
+        self.power_output = power_output
+
+#    def wind_park_p_curve(self):
+#        p_curve = np.sum([self.wind_turbines[i].power_curve
+#                          for i in range(len(self.wind_turbines))], axis=0)
+#        return p_curve
