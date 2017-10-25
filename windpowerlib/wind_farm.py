8c28a9d656047180667aa2fba551549d998fd7ef
SabineHaas <sabine.haas@rl-institut.de>
2017-10-25 17:16:45 +0200

    Add docstring

--- windpowerlib/wind_farm.py
+++ windpowerlib/wind_farm.py
@@ -12,8 +12,38 @@ import numpy as np
 
 
 class WindFarm(object):
+    r"""
+    Defines a standard set of wind farm attributes.
+
+    Parameters
+    ----------
+    wind_farm_name : string
+        Name of the wind farm.
+    wind_turbine_fleet : List of Dictionaries
+        Wind turbines of wind farm. Dictionaries must have 'wind_turbine'
+        (contains wind turbine object) and 'number_of_turbines' (number of
+        turbine type in wind farm) as keys.
+    coordinates : List
+        List of coordinates [lat, lon] of location for loading data.
+
+    Attributes
+    ----------
+    wind_farm_name : string
+        Name of the wind farm.
+    wind_turbine_fleet : List of Dictionaries
+        Wind turbines of wind farm. Dictionaries must have 'wind_turbine'
+        (contains wind turbine object) and 'number_of_turbines' (number of
+        turbine type in wind farm) as keys.
+    coordinates : List
+        List of coordinates [lat, lon] of location for loading data.
+    power_curve : pandas.DataFrame or None
+        Power curve of the wind turbine. DataFrame has 'wind_speed' and
+        'values' columns with wind speeds in m/s and the corresponding power
+        curve value in W.
+    power_output : pandas.Series
+        The calculated power output of the wind turbine.
+
     """
-    
     def __init__(self, wind_farm_name, wind_turbine_fleet, coordinates):
 
         self.wind_farm_name = wind_farm_name
