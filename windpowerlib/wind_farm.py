Correct average hub height function

windpowerlib/wind_farm.py
@@ -75,8 +75,12 @@ class WindFarm(object):
         .. [1]
 
         """
-        return sum(np.log(wind_dict['wind_turbine'].hub_height ) *
-                   wind_dict['wind_turbine'].nominal_power /
-                   sum(wind_dict_2['wind_turbine'].nominal_power
-                       for wind_dict_2 in self.wind_turbine_fleet)
-                   for wind_dict in self.wind_turbine_fleet)
+        total_nominal_power = sum(
+            wind_dict_2['wind_turbine'].nominal_power *
+            wind_dict_2['number_of_turbines']
+            for wind_dict_2 in self.wind_turbine_fleet)
+        return np.exp(sum(np.log(wind_dict['wind_turbine'].hub_height) *
+                          wind_dict['wind_turbine'].nominal_power *
+                          wind_dict['number_of_turbines']
+                          for wind_dict in self.wind_turbine_fleet) /
+                      total_nominal_power)