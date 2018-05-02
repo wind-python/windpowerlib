37934e6cac8f8d0238bfb1a3e42a94dc35587ce1
SabineHaas <sabine.haas@rl-institut.de>
2018-05-02 17:06:00 +0200

    Adapt tests to cp density correction deletion

--- tests/test_power_output.py
+++ tests/test_power_output.py
@@ -20,7 +20,6 @@ class TestPowerOutput:
                       'power_coefficient_curve_values':
                           pd.Series([0.3, 0.4, 0.5])}
 
-        # Tests without density correction:
         # Test wind_speed as pd.Series with density and power_coefficient_curve
         # as pd.Series and np.array
         power_output_exp = pd.Series(data=[0.0, 244615.399, 0.0],
@@ -54,48 +53,6 @@ class TestPowerOutput:
         assert_allclose(power_coefficient_curve(**parameters),
                         power_output_exp)
         assert isinstance(power_coefficient_curve(**parameters), np.ndarray)
-
-        # Tests with density correction:
-        # Test wind_speed as np.array with density and power_coefficient_curve
-        # as pd.Series and np.array
-        power_output_exp = np.array([0.0, 262869.785, 0.0])
-        parameters['density_correction'] = True
-        assert_allclose(power_coefficient_curve(**parameters),
-                        power_output_exp)
-        assert isinstance(power_coefficient_curve(**parameters), np.ndarray)
-        parameters['density'] = np.array(parameters['density'])
-        assert_allclose(power_coefficient_curve(**parameters),
-                        power_output_exp)
-        assert isinstance(power_coefficient_curve(**parameters), np.ndarray)
-        parameters['power_coefficient_curve_values'] = np.array(
-            parameters['power_coefficient_curve_values'])
-        parameters['power_coefficient_curve_wind_speeds'] = np.array(
-            parameters['power_coefficient_curve_wind_speeds'])
-        assert_allclose(power_coefficient_curve(**parameters),
-                        power_output_exp)
-        assert isinstance(power_coefficient_curve(**parameters), np.ndarray)
-
-        # Test wind_speed as pd.Series with density and power_coefficient_curve
-        # as np. array and pd.Series
-        power_output_exp = pd.Series(data=[0.0, 262869.785, 0.0],
-                                     name='feedin')
-        parameters['wind_speed'] = pd.Series(data=parameters['wind_speed'])
-        assert_series_equal(power_coefficient_curve(**parameters),
-                            power_output_exp)
-        parameters['density'] = pd.Series(data=parameters['density'])
-        assert_series_equal(power_coefficient_curve(**parameters),
-                            power_output_exp)
-        parameters['power_coefficient_curve_wind_speeds'] = pd.Series(
-            data=parameters['power_coefficient_curve_wind_speeds'])
-        parameters['power_coefficient_curve_values'] = pd.Series(
-            data=parameters['power_coefficient_curve_values'])
-        assert_series_equal(power_coefficient_curve(**parameters),
-                            power_output_exp)
-
-        # Raise TypeErrors due to wrong type of `density_correction`
-        with pytest.raises(TypeError):
-            parameters['density'] = 'wrong_type'
-            power_coefficient_curve(**parameters)
 
     def test_power_curve(self):
         parameters = {'wind_speed': pd.Series(data=[2.0, 5.5, 7.0]),
