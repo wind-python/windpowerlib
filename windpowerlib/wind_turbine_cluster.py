e181fd616e25f48d4af5d53fda79be7de2f3e3e2
SabineH <sabine.haas@rl-institut.de>
2018-12-05 18:42:51 +0100

    bugfix power curve assignment

--- windpowerlib/wind_turbine_cluster.py
+++ windpowerlib/wind_turbine_cluster.py
@@ -105,6 +105,8 @@ class WindTurbineCluster(object):
             Installed power of the wind turbine cluster.
 
         """
+        for wind_farm in self.wind_farms:
+            wind_farm.installed_power = wind_farm.get_installed_power()
         return sum(wind_farm.installed_power for wind_farm in self.wind_farms)
 
     def assign_power_curve(self, wake_losses_model='power_efficiency_curve',
@@ -169,7 +171,7 @@ class WindTurbineCluster(object):
             # hub height of turbine cluster)
             farm.mean_hub_height()
             # Assign wind farm power curve
-            farm.power_curve = farm.assign_power_curve(
+            farm.assign_power_curve(
                 wake_losses_model=wake_losses_model,
                 smoothing=smoothing, block_width=block_width,
                 standard_deviation_method=standard_deviation_method,
