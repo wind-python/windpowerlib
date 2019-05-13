17152568c25749066797a67ba9863f79aa221b6c
SabineH <sabine.haas@rl-institut.de>
2019-05-13 10:20:07 +0200

    PEP8 Style

--- tests/test_wind_turbine.py
+++ tests/test_wind_turbine.py
@@ -4,6 +4,7 @@ import os
 from windpowerlib.wind_turbine import (get_turbine_data_from_file, WindTurbine,
                                        get_turbine_types)
 
+
 class TestWindTurbine:
 
     def test_error_raising(self):
@@ -16,20 +17,19 @@ class TestWindTurbine:
                                   'data_source': source}
         # Raise system exit due to turbine type not in file
         with pytest.raises(SystemExit):
-            test_turbine = WindTurbine(**self.test_turbine_data)
+            WindTurbine(**self.test_turbine_data)
 
         # Raise ValueError due to invalid parameter `fetch_curve`
         self.test_turbine_data['fetch_curve'] = 'misspelling'
         self.test_turbine_data['name'] = 'DUMMY 3'
         with pytest.raises(ValueError):
-            test_turbine = WindTurbine(**self.test_turbine_data)
+            WindTurbine(**self.test_turbine_data)
 
         # Raise KeyError due to turbine type not in oedb
         self.test_turbine_data['fetch_curve'] = 'power_curve'
         self.test_turbine_data['data_source'] = 'oedb'
         with pytest.raises(KeyError):
-            test_turbine = WindTurbine(**self.test_turbine_data)
-
+            WindTurbine(**self.test_turbine_data)
 
     def test_get_turbine_data_from_file(self):
         # Raise FileNotFoundError due to missing
