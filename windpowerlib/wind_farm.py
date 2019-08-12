6a0da43391f65eb386efe5cc9f404c78f53ee716
SabineH <sabine.haas@rl-institut.de>
2019-08-12 16:23:36 +0200

    Delete sort parameter

--- windpowerlib/wind_farm.py
+++ windpowerlib/wind_farm.py
@@ -362,14 +362,13 @@ class WindFarm(object):
                     power_curve = pd.concat(
                         [pd.DataFrame(data={
                             'value': [0.0], 'wind_speed': [0.0]}),
-                         power_curve], sort=True)
+                            power_curve])
                 if power_curve.iloc[-1]['value'] != 0.0:
                     power_curve = pd.concat(
                         [power_curve, pd.DataFrame(data={
                             'value': [0.0], 'wind_speed': [
                                 power_curve['wind_speed'].loc[
-                                    power_curve.index[-1]] + 0.5]})],
-                        sort=True)
+                                    power_curve.index[-1]] + 0.5]})])
             # Add power curves of all turbine types to data frame
             # (multiplied by turbine amount)
             df = pd.concat(
