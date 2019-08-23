895d27e24a6765baff220eb200495ee7414ebd52
uvchik <uvchik.git@posteo.eu>
2019-08-23 12:12:52 +0200

    Remove not existing parameters from docstrings

--- windpowerlib/wind_turbine_cluster.py
+++ windpowerlib/wind_turbine_cluster.py
@@ -71,11 +71,6 @@ class WindTurbineCluster(object):
         See :attr:`~.wind_turbine_cluster.WindTurbineCluster.nominal_power`
         for further information.
 
-        Parameters
-        -----------
-        nominal_power : float
-            Nominal power of the wind turbine cluster in w.
-
         Returns
         -------
         float
@@ -189,10 +184,6 @@ class WindTurbineCluster(object):
             wind turbine cluster for power curve smoothing with
             'turbulence_intensity' method. Can be calculated from
             `roughness_length` instead. Default: None.
-        roughness_length : float (optional)
-            Roughness length. If `standard_deviation_method` is
-            'turbulence_intensity' and `turbulence_intensity` is not given
-            the turbulence intensity is calculated via the roughness length.
 
         Returns
         -------
