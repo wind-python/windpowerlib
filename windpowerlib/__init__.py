08c3b61fd5af5b6e74b700dbb262f2dbcf7ff35a
uvchik <uvchik.git@posteo.eu>
2020-02-24 16:30:30 +0100

    Fix lint errors

--- windpowerlib/__init__.py
+++ windpowerlib/__init__.py
@@ -2,11 +2,9 @@ __copyright__ = "Copyright oemof developer group"
 __license__ = "MIT"
 __version__ = "0.2.1dev"
 
-from windpowerlib.wind_turbine import (
-    WindTurbine,
-    get_turbine_types,
-    create_power_curve,
-)
+from windpowerlib.wind_turbine import WindTurbine
+from windpowerlib.wind_turbine import get_turbine_types
+from windpowerlib.wind_turbine import create_power_curve
 from windpowerlib.wind_farm import WindFarm
 from windpowerlib.wind_turbine_cluster import WindTurbineCluster
 from windpowerlib.modelchain import ModelChain
