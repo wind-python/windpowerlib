e864dbbc0796059da33fa7ea2adefb33fab29f7f
uvchik <uvchik.git@posteo.eu>
2020-03-10 14:09:23 +0100

    Specify ignored error

--- windpowerlib/__init__.py
+++ windpowerlib/__init__.py
@@ -6,8 +6,8 @@ from .wind_turbine import (
     WindTurbine,
     get_turbine_types,
     create_power_curve,
-)  # noq
-from .wind_farm import WindFarm  # noq
-from .wind_turbine_cluster import WindTurbineCluster  # noq
-from .modelchain import ModelChain  # noq
-from .turbine_cluster_modelchain import TurbineClusterModelChain  # noq
+)  # noq: F401
+from .wind_farm import WindFarm  # noq: F401
+from .wind_turbine_cluster import WindTurbineCluster  # noq: F401
+from .modelchain import ModelChain  # noq: F401
+from .turbine_cluster_modelchain import TurbineClusterModelChain  # noq: F401
