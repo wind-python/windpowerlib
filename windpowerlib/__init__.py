9c5901a8f0e8e00be99a24ce897d5b91325cc6b1
uvchik <uvchik.git@posteo.eu>
2020-03-10 14:18:06 +0100

    Fix typo in comment

--- windpowerlib/__init__.py
+++ windpowerlib/__init__.py
@@ -6,8 +6,8 @@ from .wind_turbine import (
     WindTurbine,
     get_turbine_types,
     create_power_curve,
-)  # noq: F401
-from .wind_farm import WindFarm  # noq: F401
-from .wind_turbine_cluster import WindTurbineCluster  # noq: F401
-from .modelchain import ModelChain  # noq: F401
-from .turbine_cluster_modelchain import TurbineClusterModelChain  # noq: F401
+)  # noqa: F401
+from .wind_farm import WindFarm  # noqa: F401
+from .wind_turbine_cluster import WindTurbineCluster  # noqa: F401
+from .modelchain import ModelChain  # noqa: F401
+from .turbine_cluster_modelchain import TurbineClusterModelChain  # noqa: F401
