82368434853a354fa475e2cc3a337331eed262f8
uvchik <uvchik.git@posteo.eu>
2020-04-07 09:16:53 +0200

    Add module for data handling
    It might be a good idea to copy some other data-functions to this
    module.

---
+++ tests/test_data_handling.py
@@ -0,0 +1,10 @@
+"""
+SPDX-FileCopyrightText: 2019 oemof developer group <contact@oemof.org>
+SPDX-License-Identifier: MIT
+"""
+
+from windpowerlib import data
+
+
+def test_integretiy_check():
+    data.check_data_integretiy()
---
+++ windpowerlib/data.py
@@ -0,0 +1,10 @@
+"""
+The ``data`` module contains functions to handle the needed data.
+
+SPDX-FileCopyrightText: 2019 oemof developer group <contact@oemof.org>
+SPDX-License-Identifier: MIT
+"""
+
+
+def check_data_integretiy():
+    pass
